6901aa8be05bd7b6146b275aef5925db06c2a1e7
Jobin T v <jobintv@focaloid.com>
2022-11-25 13:27:05 +0530

    feat: Age function completed

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,9 +4,13 @@ import { DateTime } from 'luxon';
 @Injectable()
 export class AppService {
   calulateAgeWithBirthDate(birthDateDto: { date: string }): { data: string } {
-    const birthDate = DateTime.fromISO(birthDateDto);
-    const currentDate = DateTime.fromISO(Date.now());
-    const age = birthDate.diff(currentDate);
-    return { data: `${birthDateDto.date} age:12` };
+    return {
+      data: `Your age is ${Math.abs(
+        Math.ceil(
+          DateTime.fromFormat(birthDateDto.date, 'dd/MM/yyyy').diffNow('years')
+            .years,
+        ),
+      )}`,
+    };
   }
 }
